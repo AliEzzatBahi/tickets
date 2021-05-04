a52bff86fba42fbc91352490e7545f9104fa0eb3
AliEzzatBahi <aliezzatbahi@gmail.com>
2021-05-04 12:51:12 +0200

    Blogs Migration

---
+++ tickets/config/Seeds/BlogsSeed.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+
+/**
+ * Users seed.
+ */
+class BlogsSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id' => '1',
+                'title' => 'Post Title One',
+                'post' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                'image' => '#',
+                'user_id' => '1',
+                'created' => '2021-04-14 08:24:31',
+                'modified' => '2021-04-14 08:24:31',
+            ],
+        ];
+
+        $table = $this->table('blogs');
+        $table->insert($data)->save();
+    }
+}
